54cd77d6572637f582520ed3ef3df8965b2f3528
Pamir Perdeci <pamirperdeci@gmail.com>
2022-11-18 23:24:56 -0500

    added color change effect on nav hover

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './less/index.less'
 
 // Your code goes here!
+const mainNav = document.querySelector(".main-navigation");
+
+mainNav.addEventListener('mouseenter', (evt)=> {
+    mainNav.setAttribute("style","background-color: #FAE3D1;");
+})
+
+mainNav.addEventListener('mouseleave', (evt)=>{
+    mainNav.setAttribute("style", "background-color: white;");
+})
