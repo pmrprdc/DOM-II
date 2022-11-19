edb7dbfea0bb35de0369e63afb5970e06ad27149
Pamir Perdeci <pamirperdeci@gmail.com>
2022-11-19 18:38:39 -0500

    scroll event listener changes the background color of blog

--- src/index.js
+++ src/index.js
@@ -10,3 +10,16 @@ mainNav.addEventListener('mouseenter', (evt)=> {
 mainNav.addEventListener('mouseleave', (evt)=>{
     mainNav.setAttribute("style", "background-color: white;");
 })
+
+/// Changing background color on scroll
+
+window.addEventListener("scroll", function changeBg(){
+    const scrollable = document.documentElement.scrollHeight - this.window.innerHeight;
+    console.log(window.scrollY);
+    console.log(scrollable);
+    if (window.scrollY === scrollable){
+    this.document.body.style.backgroundColor = "#ffc6d9"
+    } else if(this.window.scrollY >300 && this.window.scrollY < 500 ){
+        this.document.body.style.backgroundColor = "#FFE1C6"
+    }
+})
